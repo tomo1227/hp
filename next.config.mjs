c88677d71f42b685e9fced2e07cb470b6bd325d3
tomo1227 <tomo.soccer1227@gmail.com>
2024-11-26 14:47:45 +0000

    環境変数の設定変更

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,6 @@ const nextConfig = {
             "tomokiota-photos.s3.ap-northeast-1.amazonaws.com",
         ],
     },
-    env: {
-        GMAIL_USER: process.env.GMAIL_USER,
-        GMAIL_APP_PASS: process.env.GMAIL_APP_PASS,
-    },
 };
 
 export default nextConfig;
