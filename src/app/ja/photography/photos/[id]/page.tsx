fc2228e7c0f4f63f79ec228473d83f3bfbcb994c
tomo1227 <tomo.soccer1227@gmail.com>
2024-11-28 11:57:24 +0000

    photoにimage galleryを追加

---
+++ src/app/ja/photography/photos/[id]/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+    const slugs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    return slugs.map((slug) => ({ id: slug }));
+}
+
+export default async function PhotoPage({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const id = (await params).id;
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <Image
+                src={`/img/photography/img${id}.jpg`}
+                alt="travel-jpg"
+                width={500}
+                height={500}
+            />
+        </div>
+    );
+}
