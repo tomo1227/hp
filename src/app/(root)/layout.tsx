30a5eb36eb8575fb1c490a5c98646e4025d3cedf
tomo1227 <tomo.soccer1227@gmail.com>
2024-12-04 10:31:30 +0000

    loading時にプログレスバーを追加

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = localFont({
     src: "../fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
+                <NextTopLoader />
                 {children}
             </body>
         </html>
