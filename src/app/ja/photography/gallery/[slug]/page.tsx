77b5c42de776acb7c5dd4d9b996d650f17957a6b
tomo1227 <tomo.soccer1227@gmail.com>
2024-12-05 19:14:41 +0000

    blogの個別ページを編集

---
+++ src/app/ja/photography/gallery/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { getFilteredPosts, getPostBySlug } from "../../../../../lib/blogFilter";
+// import { formattedDate } from "../../../../lib/date";
+
+export async function generateStaticParams() {
+  const posts = await getFilteredPosts("desc", "ja", "photography", "gallery");
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const { Component, frontmatter } = await getPostBySlug(slug, "ja");
+  return (
+    <article
+      className="markdown flex flex-col justify-center items-center"
+      style={{ position: "relative" }}
+    >
+      <Component />
+    </article>
+  );
+}
---
+++ src/app/ja/photography/gallery/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Fragment } from "react";
+import { getFilteredPosts } from "../../../../lib/blogFilter";
+import { formattedDate } from "../../../../lib/date";
+
+export default async function Page() {
+  const posts = await getFilteredPosts("desc", "ja");
+  return (
+    <article
+      className="markdown flex flex-col justify-center items-center"
+      style={{ position: "relative" }}
+    >
+      {posts.map((post) => (
+        <Fragment key={post.slug}>
+          <h1>{post.data.title}</h1>
+          <Link href={`/ja/photography/gallery/${post.slug}`}>
+            {formattedDate(post.data.date)}
+          </Link>
+        </Fragment>
+      ))}
+    </article>
+  );
+}
