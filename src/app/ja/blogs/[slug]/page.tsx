ddcfcb0403d46b6a270d48f01caab13a5c6bd15e
tomo1227 <tomo.soccer1227@gmail.com>
2024-12-06 00:37:08 +0000

    pathを修正

--- src/app/ja/blogs/[slug]/page.tsx
+++ src/app/ja/blogs/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default async function Page({
 }) {
   const slug = (await params).slug;
   const { frontmatter } = await getPostBySlug(slug, "ja");
-  const Component = require(`../../../../_posts/ja/${slug}.mdx`).default;
+  const Component = require(`@/_posts/ja/${slug}.mdx`).default;
   const splitedTitle = parser.parse(frontmatter.title);
   return (
     <div>
