0ec2f61ba7e8534a795de082574b7c9e68fe514f
tomo1227 <tomo.soccer1227@gmail.com>
2025-01-17 16:44:22 +0000

    淡路島のギャラリーを追加

--- src/cmd/generateBlogImages.mjs
+++ src/cmd/generateBlogImages.mjs
@@ -1,15 +1,36 @@
 import { writeFileSync } from "node:fs";
 
-const baseURL =
-  "https://tomokiota-photos.s3.ap-northeast-1.amazonaws.com/2024/calgary/";
-const imageCount = 55;
+import readline from "node:readline";
 
-let output = "";
-for (let i = 1; i <= imageCount; i++) {
-  const tag = `<BlogImage src="${baseURL}img${i}.jpg" width={1000} />\n`;
-  output += tag;
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-writeFileSync("./output.txt", output, "utf8");
+rl.question("Base URLを入力してください(末尾/あり): ", (baseURL) => {
+  rl.question("画像の数を入力してください: ", (imageCount) => {
+    let output = "";
+    for (let i = 1; i <= Number.parseInt(imageCount, 10); i++) {
+      const tag = `<BlogImage src="${baseURL}img${i}.jpg" width={1000} />\n`;
+      output += tag;
+    }
 
-console.log("ブログ画像タグが生成されました！");
+    writeFileSync("./output.txt", output, "utf8");
+    console.log("./output.txt が生成されました！");
+    rl.close();
+  });
+});
+
+// const baseURL =
+//   "https://tomokiota-photos.s3.ap-northeast-1.amazonaws.com/2024/awaji-island/";
+// const imageCount = 38;
+
+// let output = "";
+// for (let i = 1; i <= imageCount; i++) {
+//   const tag = `<BlogImage src="${baseURL}img${i}.jpg" width={1000} />\n`;
+//   output += tag;
+// }
+
+// writeFileSync("./output.txt", output, "utf8");
+
+// console.log("Success to create ./output.txt が生成されました！");
