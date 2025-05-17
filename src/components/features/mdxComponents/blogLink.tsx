e578c5dc9cf9d994f1c9a7d312b57481bec4e343
tomo1227 <tomo.soccer1227@gmail.com>
2025-05-17 04:25:03 +0000

    blogのリンクのスタイルを変更する

--- src/components/features/mdxComponents/blogLink.tsx
+++ src/components/features/mdxComponents/blogLink.tsx
@@ -13,6 +13,7 @@ export const BlogLink: React.FC<BlogLinkProps> = ({
   return (
     <Link
       {...props}
+      className="text-blue-500 dark:text-blue-400 hover:underline mx-1 transition-colors duration-300 hover:text-blue-700 dark:hover:text-blue-300"
       target={openNewTab ? "_blank" : undefined}
       rel={openNewTab ? "noopener noreferrer" : undefined}
     >
