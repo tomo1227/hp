12caefcbdef9cc0cfcbc4ca272f0a69b18e84087
tomo1227 <tomo.soccer1227@gmail.com>
2025-05-18 14:24:18 +0000

    robots.tsの作成

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/ja/gallery/*", "/en/gallery/*"],
+    },
+    sitemap: "https://tomokiota.com/sitemap.xml",
+  };
+}
