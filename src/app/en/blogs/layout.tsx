e9440855e1d03e64c6ac4b80f1ad97e00278ce19
tomo1227 <tomo.soccer1227@gmail.com>
2026-09-02 06:24:20 +0000

    blogにToCの追加

--- src/app/en/blogs/layout.tsx
+++ src/app/en/blogs/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div id="blog-layout">
       <SidebarLeft locale="en" />
       <div id="main-blog-contents">{children}</div>
-      <SidebarRight locale="en" />
+      <SidebarRight locale="en" showToc />
     </div>
   );
 }
--- src/components/ui/sidebarRight.tsx
+++ src/components/ui/sidebarRight.tsx
@@ -1,16 +1,19 @@
+import { TableOfContents } from "@/components/features/tableOfContents";
 import SidebarMemberActions from "@/components/ui/sidebarMemberActions";
 
 type SidebarRightProps = {
   locale: "ja" | "en";
+  showToc?: boolean;
 };
 
-const SidebarRight = ({ locale }: SidebarRightProps) => {
+const SidebarRight = ({ locale, showToc = false }: SidebarRightProps) => {
   return (
     <div className="sidebar-container sidebar-right">
-      <div className="desktop-sidebar">
+      <div className="desktop-sidebar sidebar-right-inner">
         <ul className="sidebar-list sidebar-actions">
           <SidebarMemberActions locale={locale} />
         </ul>
+        {showToc && <TableOfContents locale={locale} />}
       </div>
     </div>
   );
